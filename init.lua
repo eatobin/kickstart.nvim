4d1a17007e771c6ad0a445931791fc6e355b8d57
Eric Tobin <eatobin@gmail.com>
2024-10-07 17:50:49 -0700

    update

--- init.lua
+++ init.lua
@@ -830,8 +830,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    lazy = false, -- Make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- Make sure to load this before all the other start plugins
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
